d905e0acc1f5583df86fabf18c0de8fddd618483
mohamedashiq26 <ashiq.26.2005@gmail.com>
2025-03-02 22:56:59 +0530

    Create Nested Loop Increment.java

---
+++ Nested Loop Increment.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        int c=10;
+        for (int i=1;i<=6;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                c=c+3;
+                
+            }
+        }
+        System.out.println(c);
+    }
+}
