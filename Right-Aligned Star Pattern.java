7995040ee39d168f1d7cebe787a965e61335a2f5
mohamedashiq26 <ashiq.26.2005@gmail.com>
2025-03-02 22:52:16 +0530

    Create Right-Aligned Star Pattern.java

---
+++ Right-Aligned Star Pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+
+        scanner.close(); 
+    }
+}
