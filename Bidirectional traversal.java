bb2d1a2578da3f1d82b734070340585527b118f3
mohamedashiq26 <ashiq.26.2005@gmail.com>
2025-03-02 23:10:19 +0530

    Create Bidirectional traversal.java

---
+++ Bidirectional traversal.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        for(int i=1,j=499;i<=j;i++,j--){
+            System.out.println(i);
+            System.out.println(j);
+        }
+    }
+}
