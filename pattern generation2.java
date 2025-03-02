2ee4bdf6b45ebb76f6dfa9b5326dddb2dd11106c
mohamedashiq26 <ashiq.26.2005@gmail.com>
2025-03-02 22:46:22 +0530

    Create pattern generation2.java

---
+++ pattern generation2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            // Print stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
